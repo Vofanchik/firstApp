892625c9296dc187fdb2f0571378983f4465d0f1
Vladimir Kalinin <Vofamchic@mail.ru>
2024-09-23 19:12:14 +0300

    auth ui defender added

---
+++ resources/views/defender.blade.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ID: {{$user->id}}</h1>
+    <h1>Имя: {{$user->name}}</h1>
+    <h1>Почта: {{$user->email}}</h1>
+</body>
+</html>
